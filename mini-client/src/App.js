668ea3dc607a9865faed81867d42a661d656ae35
gosaintmrc <gosaintmrc@aliyun.com>
2025-09-21 21:38:52 +0800

    页面布局基本完成

--- mini-client/src/App.js
+++ mini-client/src/App.js
@@ -1,4 +1,15 @@
-import {Box, Collapse, List, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography} from "@mui/material";
+import {
+    AppBar,
+    Box, Button,
+    Collapse,
+    IconButton,
+    List,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Toolbar,
+    Typography
+} from "@mui/material";
 import {
     AssignmentLate as AssignmentLateIcon,
     BarChart as BarChartIcon,
@@ -13,11 +24,13 @@ import {
     Store as StoreIcon,
     Warehouse as WarehouseIcon,
 } from '@mui/icons-material';
+import MenuIcon from '@mui/icons-material/Menu';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 import FireTruckIcon from '@mui/icons-material/FireTruck';
 import LockIcon from '@mui/icons-material/Lock';
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {menuAPI} from "./api/APIService";
 
 
@@ -48,7 +61,7 @@ const getMenuIcon = (iconName) => {
 };
 
 /** 递归菜单的实现*/
-const RecursiveMenu = ({menu}) => {
+const RecursiveMenu = React.memo(({menu}) => {
     /** 控制菜单打开 */
     const [open, setOpen] = useState(false);
     const hasChildren = menu.children && menu.children.length > 0;
@@ -84,7 +97,8 @@ const RecursiveMenu = ({menu}) => {
             )}
         </>
     );
-};
+})
+
 
 function App() {
     /** 菜单的加载状态 默认加载中*/
@@ -110,6 +124,8 @@ function App() {
     }, [])
     return (
         <Box sx={{display: 'flex', minHeight: '100vh', flexDirection: 'row'}}>
+
+            {/*侧边栏*/}
             <Box sx={{bgcolor: 'grey.900', color: 'grey.400', boxSizing: 'border-box'}}>
                 <Toolbar sx={{my: 4}}>
                     <Box display="flex" alignItems="center">
@@ -133,7 +149,41 @@ function App() {
                     }
                 </List>
             </Box>
-            <Box sx={{bgcolor: 'red'}}>Test</Box>
+            <Box sx={{flexGrow:1,minHeight:'100vh',display:'flex',flexDirection:'column'}}>
+                <AppBar position="fixed" color="transparent" elevation={0} sx={{ borderBottom: '1px solid', borderColor: 'divider', transition: 'margin 225ms cubic-bezier(0.4, 0, 0.2, 1), width 225ms cubic-bezier(0.4, 0, 0.2, 1)' }}>
+                    <Toolbar sx={{ justifyContent: 'space-between', p: 2 }}>
+                        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                            <IconButton edge="start"  aria-label="menu" sx={{ mr: 1 }}>
+                                <MenuIcon />
+                            </IconButton>
+                           面包屑
+                        </Box>
+                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                            <Typography variant="body2" color="text.secondary" sx={{ mr: 1 }}>
+                                主题
+                            </Typography>
+                            <Button  sx={{ minWidth: 'auto', p: 1, borderRadius: '50%' }}>
+                                <DarkModeIcon fontSize="small" />
+                            </Button>
+                        </Box>
+                    </Toolbar>
+                </AppBar>
+
+                <Toolbar />
+
+                <Box sx={{
+                    flex: 1,
+                    px: { xs: 1, sm: 2, md: 3 },
+                    py: 2,
+                    width: '100%'
+                }}>
+                    <Box sx={{
+                        width: '100%'
+                    }}>
+                       <h1>子页面在此渲染</h1>
+                    </Box>
+                </Box>
+            </Box>
         </Box>
     );
 }
